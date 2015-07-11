98ea9b02b61c83e375c8cffcdf494d5a74478ec0
Dima Sukhikh <coddeys@gmail.com>
2015-07-11 15:18:47 +0300

    temp commit

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,8 @@
 class Subscription < ActiveRecord::Base
   belongs_to :user
+
+  validates :first_name, :last_name, :street_address,
+            :unit, :city , :country, :postal_code,
+            presence: true
+
 end
