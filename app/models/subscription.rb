82f4e2bbe67b47ff75a240ce654a115e408307a9
Dmitry Sukhikh <coddeys@gmail.com>
2015-07-12 00:24:24 +0300

    rails g migration add_stripe_to_subscriptions

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -6,4 +6,12 @@ class Subscription < ActiveRecord::Base
 
   attr_accessor :stripe_card_token
 
+  def save_with_payment
+    if valid?
+      user = Stripe::Customer.create(description:email, card: stripe_card_token)
+      self.stripe_customer_token = user.id
+      save!
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150711114700) do
+ActiveRecord::Schema.define(version: 20150711212408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,8 +26,9 @@ ActiveRecord::Schema.define(version: 20150711114700) do
     t.string   "country"
     t.string   "postal_code"
     t.integer  "user_id"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+    t.string   "stripe_customer_token"
   end
 
   add_index "subscriptions", ["user_id"], name: "index_subscriptions_on_user_id", using: :btree
