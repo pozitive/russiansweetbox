b0669f458925a89ca6e313a21305dd176beb1d57
Dmitry Sukhikh <coddeys@gmail.com>
2015-07-12 16:48:13 +0300

    Your subscription path

--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -1,2 +1,11 @@
 module SubscriptionsHelper
+
+  def user_subscription_path(current_user)
+    if current_user.subscriptions.any?
+      subscription_path(current_user.subscriptions.first)
+    else
+      root_path
+    end
+  end
+  
 end
