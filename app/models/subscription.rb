442bfbb8a2b93362ebfee7e2b5fc97fcb70d243c
Dmitry Sukhikh <coddeys@gmail.com>
2015-07-12 16:10:29 +0300

    25$ per month

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,13 +2,17 @@ class Subscription < ActiveRecord::Base
   belongs_to :user
 
   validates :first_name, :last_name, :street_address,
-            :city , :country, :unit, presence: true
+            :city , :country, presence: true
 
   attr_accessor :stripe_card_token
 
   def save_with_payment
     if valid?
-      user = Stripe::Customer.create(description: first_name, card: stripe_card_token)
+      user = Stripe::Customer.create(
+        description: first_name,
+        plan:       'simple', 
+        card:       stripe_card_token
+      )
       self.stripe_customer_token = user.id
       save!
     end
